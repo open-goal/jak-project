50814f8529747694bf530ed3cbd59ec44d7fc993
Tyler Wilding <xtvaser@gmail.com>
2020-08-26 00:06:13 -0400

    Add Github Actions and cleanup most test spam

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export NEXT_DIR=$DIR
-$DIR/build/test/goalc-test "$@"
+$DIR/build/test/goalc-test --gtest_color=yes --gtest_brief=1 "$@"
