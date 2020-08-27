8a81ff78dd9131f6b15b5e18e93e8e98b1256795
doctaweed <70249609+doctaweed@users.noreply.github.com>
2020-08-26 20:23:22 -0600

    Windows fixes
    Partially addresses issues with unresolved external symbols

--- goalc/listener/Deci2Server.cpp
+++ goalc/listener/Deci2Server.cpp
@@ -1,8 +1,12 @@
+
+
+
+
 /*!
  * @file Deci2Server.cpp
  * Basic implementation of a DECI2 server.
  * Works with deci2.cpp (sceDeci2) to implement the networking on target
- */
+ 
 
 #include <cstdio>
 #include <sys/socket.h>
@@ -41,7 +45,7 @@ Deci2Server::~Deci2Server() {
 
 /*!
  * Start waiting for the Listener to connect
- */
+ 
 bool Deci2Server::init() {
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
@@ -103,7 +107,7 @@ bool Deci2Server::init() {
 
 /*!
  * Return true if the listener is connected.
- */
+ 
 bool Deci2Server::check_for_listener() {
   if (server_connected) {
     if (accept_thread_running) {
@@ -118,7 +122,7 @@ bool Deci2Server::check_for_listener() {
 
 /*!
  * Send data from buffer. User must provide appropriate headers.
- */
+ 
 void Deci2Server::send_data(void* buf, u16 len) {
   lock();
   if (!server_connected) {
@@ -139,14 +143,14 @@ void Deci2Server::send_data(void* buf, u16 len) {
 
 /*!
  * Lock the DECI mutex. Should be done before modifying protocols.
- */
+ 
 void Deci2Server::lock() {
   deci_mutex.lock();
 }
 
 /*!
  * Unlock the DECI mutex. Should be done after modifying protocols.
- */
+
 void Deci2Server::unlock() {
   deci_mutex.unlock();
 }
@@ -154,7 +158,7 @@ void Deci2Server::unlock() {
 /*!
  * Wait for protocols to become ready.
  * This avoids the case where we receive messages before protocol handlers are set up.
- */
+ 
 void Deci2Server::wait_for_protos_ready() {
   if (protocols_ready)
     return;
@@ -167,7 +171,7 @@ void Deci2Server::wait_for_protos_ready() {
  * Will unblock wait_for_protos_ready and incoming messages will be dispatched to these
  * protocols.  You can change the protocol handlers, but you should lock the mutex before
  * doing so.
- */
+ 
 void Deci2Server::send_proto_ready(Deci2Driver* drivers, int* driver_count) {
   lock();
   d2_drivers = drivers;
@@ -249,7 +253,7 @@ void Deci2Server::run() {
 
 /*!
  * Background thread for waiting for the listener.
- */
+ 
 void Deci2Server::accept_thread_func() {
   socklen_t l = sizeof(addr);
   while (!kill_accept_thread) {
@@ -262,3 +266,4 @@ void Deci2Server::accept_thread_func() {
     }
   }
 }
+*/
