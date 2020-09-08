8f1ddca8256e33882b24541fc75365d3e9aa06ff
Tyler Wilding <xtvaser@gmail.com>
2020-09-08 14:39:16 -0400

    Add Coveralls.io

--- test-cov.sh
+++ test-cov.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export NEXT_DIR=$DIR
 export FAKE_ISO_PATH=/game/fake_iso.txt
-cd $DIR/build/
-make goalc-test_coverage
-ls
-ls ..
+cd $DIR/build/test
+make init
+make gcov
+make lcov
