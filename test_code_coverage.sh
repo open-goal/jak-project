c9e99b8ebe42f582852d7dfbab420186492a43a4
water <awaterford111445@gmail.com>
2020-09-08 19:15:38 -0400

    add a separate cmake configuration for code coverage builds

--- test_code_coverage.sh
+++ test_code_coverage.sh
@@ -5,4 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export NEXT_DIR=$DIR
 export FAKE_ISO_PATH=/game/fake_iso.txt
-$DIR/build/test/goalc-test --gtest_color=yes "$@"
+cd $DIR/build/
+make goalc-test_coverage
