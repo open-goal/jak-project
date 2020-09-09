3973d2ef4fa2d042ed49a8c85dcb7f4398d67a24
Shay <shabennett2@gmail.com>
2020-09-09 15:28:05 -0600

    Remove build folder
    Remove build folder in asset_tool

--- goalc/main.cpp
+++ goalc/main.cpp
@@ -1,12 +1,10 @@
 #include <cstdio>
 #include "goalc/compiler/Compiler.h"
-#include "third-party/spdlog/include/spdlog/spdlog.h"
-#include "third-party/spdlog/include/spdlog/fmt/fmt.h"
+
 
 int main(int argc, char** argv) {
   (void)argc;
-  (void)argv;
-  spdlog::debug("This is a spdlog debug message");    
+  (void)argv;  
   printf("goal compiler\n");
 
   Compiler compiler;
