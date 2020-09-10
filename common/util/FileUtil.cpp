369a1031e12193abc91e110aca352b4ea7d9ad70
blahpy <dexhowe@yahoo.co.uk>
2020-09-10 22:07:23 +1200

    Update functions

--- common/util/FileUtil.cpp
+++ common/util/FileUtil.cpp
@@ -3,22 +3,31 @@
 #include <stdio.h> /* defines FILENAME_MAX */
 
 #ifdef _WIN32
-#include <direct.h>
-#define GetCurrentDir _getcwd
+#include <Windows.h>
 #else
 #include <unistd.h>
-#define GetCurrentDir getcwd
 #endif
 
-std::string FileUtil::GetCurrentWorkingDir() {
-  char buff[FILENAME_MAX];
-  GetCurrentDir(buff, FILENAME_MAX);
-  std::string current_working_dir(buff);
-  return current_working_dir;
+std::string FileUtil::GetExecutablePath() {
+#ifdef _WIN32
+  char buffer[FILENAME_MAX];
+  GetModuleFileNameA(NULL, buffer, FILENAME_MAX);
+  std::string::size_type pos =
+      std::string(buffer).find_last_of("/\\");  // Strip executable from file path
+  return std::string(buffer).substr(0, pos);
+#else  // do Linux stuff
+  char buffer[FILENAME_MAX];
+  readlink("/proc/self/exe", buffer,
+           FILENAME_MAX);  // /proc/self acts like a "virtual folder" containing information about
+                           // the current process
+  std::string::size_type pos =
+      std::string(buffer).find_last_of("/\\");  // Strip executable from file path
+  return std::string(buffer).substr(0, pos);
+#endif
 }
 
 std::string FileUtil::get_file_path(const std::vector<std::string>& input) {
-  std::string currentPath = FileUtil::GetCurrentWorkingDir();  // std::filesystem::current_path();
+  std::string currentPath = FileUtil::GetExecutablePath();
   char dirSeparator;
 
 #ifdef _WIN32
--- common/util/FileUtil.h
+++ common/util/FileUtil.h
@@ -3,6 +3,6 @@
 #include <vector>
 
 namespace FileUtil {
-std::string GetCurrentWorkingDir();
-std::string get_file_path(std::vector<std::string> input);
+std::string GetExecutablePath();
+std::string get_file_path(const std::vector<std::string>& input);
 }  // namespace FileUtil
