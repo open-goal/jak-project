ec64e06e9e62b121cb8c321355a9d9f7ce90c461
doctashay <shabennett2@gmail.com>
2020-10-02 14:12:41 -0600

    Update for linux
    I should really fix my WSL environment

--- third-party/spdlog/include/spdlog/sinks/ansicolor_sink-inl.h
+++ third-party/spdlog/include/spdlog/sinks/ansicolor_sink-inl.h
@@ -4,11 +4,11 @@
 #pragma once
 
 #ifndef SPDLOG_HEADER_ONLY
-#include <spdlog/sinks/ansicolor_sink.h>
+#include <third-party/spdlog/include/spdlog/sinks/ansicolor_sink.h>
 #endif
 
-#include <spdlog/pattern_formatter.h>
-#include <spdlog/details/os.h>
+#include <third-party/spdlog/include/spdlog/pattern_formatter.h>
+#include <third-party/spdlog/include/spdlog/details/os.h>
 
 namespace spdlog {
 namespace sinks {
