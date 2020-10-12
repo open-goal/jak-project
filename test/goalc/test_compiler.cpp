42bc2106e5e7c4211b94b84e9772f12d6b90273a
Shay <shabennett2@gmail.com>
2020-10-12 17:38:57 -0600

    Fixes

---
+++ test/goalc/test_compiler.cpp
@@ -0,0 +1,11 @@
+#include <thread>
+#include <chrono>
+
+#include "gtest/gtest.h"
+#include "game/runtime.h"
+#include "goalc/listener/Listener.h"
+#include "goalc/compiler/Compiler.h"
+
+TEST(CompilerAndRuntime, ConstructCompiler) {
+  Compiler compiler;
+}
