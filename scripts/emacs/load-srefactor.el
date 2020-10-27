4cfe412a9cfb3a9aec618a650520d1115301564f
Tyler Wilding <xtvaser@gmail.com>
2020-10-26 21:01:10 -0400

    Handle lisp formatting almost entirely in emacs (except kicking off the jobs, emacs has no threads!)

--- scripts/emacs/load-srefactor.el
+++ scripts/emacs/load-srefactor.el
@@ -1,4 +1,60 @@
-(add-to-list 'load-path "./third-party/srefactor")
+;; Run with > emacs -batch -l ./scripts/emacs/load-srefactor.el <total processes> <process index>
 
+;; Load srefactor
+;; NOTE - we have modified it in a few ways to better support the GOAL LISP dialect
+(add-to-list 'load-path "./third-party/srefactor")
 (require 'srefactor)
 (require 'srefactor-lisp)
+;; Load benchmark.el for benchmarking
+(add-to-list 'load-path "./third-party/benchmark")
+(require 'benchmark)
+
+(defvar total-processes (string-to-number (nth 3 command-line-args)))
+(defvar process-index (string-to-number (nth 4 command-line-args)))
+
+;; Load all *.gs, *.gd, and *.gc files
+(defun load-goal-files ()
+  (directory-files-recursively "." ".*\\.g[cds]$"))
+
+(defun batch-files (files batch-size batch-index)
+  (nth batch-index
+       (seq-partition files
+		      (/ (length files)
+			 batch-size))))
+
+(defvar goal-files (load-goal-files))
+
+;; ---- Test
+;; (pp (batch-files goal-files total-processes process-index))
+
+(defun format-file (file)
+  ;; Insert file into current buffer
+  (find-file file)
+  ;; Format with srefactor
+  (srefactor-lisp-format-buffer)
+  ;; Save current buffer to file
+  (save-buffer)
+  (kill-buffer)
+  ;; (message (format "Formatted - %s" file)
+  )
+
+;; ---- TEST
+;; (defvar test "./scripts/emacs/load-srefactor.el")
+;; (pp (format "Formatting Took - %.2f seconds"
+;; 	    (benchmark-elapse (format-file test))))
+;; ----
+
+;; TODO - make this faster by working through the files in batches in multiple emacs processes
+;; srefactor only works on the current buffer, so it's pointless to open multiple files at a time in a single process
+;; Iterate through all of them!
+
+(defvar files-to-format (batch-files goal-files total-processes process-index))
+
+(message (format "[EMACS-%d] Formatting %d goal files (*.gs, *.gc, *.gd)"
+		 process-index
+		 (length files-to-format)))
+
+(pp (format "[EMACS-%d] Formatting Took - %.2f seconds"
+	    process-index
+	    (benchmark-elapse (mapc 'format-file files-to-format))))
+
--- scripts/format-lisp.ps1
+++ scripts/format-lisp.ps1
@@ -1,48 +1,6 @@
-# Assumed to be ran from the root directory
-# this formats all *.gs and *.gc files in the repository!
-# TODO:
-# - dry-run (CI check for formatting)
-# - fix mode (actually apply formatting)
-
-$gcFiles = Get-ChildItem $PWD -name -recurse *.gc
-$gsFiles = Get-ChildItem $PWD -name -recurse *.gs
-$lispFiles = $gcFiles + $gsFiles
-
-# TODO - batch prints (always print errors, print success in increments to reduce spam but still show progress)
-
-# Fix any lines that start with a single `;` change them to a line comment convention of `;;`
-Write-Host -foregroundColor yellow "Modifying all line-comments that begin with a single semi-colon"
-(Measure-Command {
-  $lispFiles | ForEach-Object -Parallel {
-    $file = $_
-    $content = Get-Content $file
-    # TODO - this isn't the best way, but this is only a temporary workaround
-    # until we fix the output to not use single ; line comments
-    $i = 0
-    for ($i = 0; $i -lt $content.Count ; $i++) {
-      if ($content[$i] -match "^;{1}[^;].*") {
-        $content[$i] = ";" + $content[$i]
-      }
-    }
-    Set-Content -Path $file -Value $content
-  }
-}).TotalMilliseconds
-
-# Test Case
-# $expr = "emacs -batch -l ./scripts/emacs/load-srefactor.el ./decompiler/config/all-types.gc --eval '(srefactor-lisp-format-buffer)' -f save-buffer"
-# $expr += ';$LastExitCode'
-# $exitCode = Invoke-Expression $expr
-Write-Host -foregroundColor yellow "Running all files through emac's formatting.  This will take a while!"
-(Measure-Command {
-  $lispFiles | ForEach-Object -Parallel {
-    $file = $_
-    $expr = "emacs -batch -l ./scripts/emacs/load-srefactor.el $($file) --eval '(srefactor-lisp-format-buffer)' -f save-buffer"
-    $expr += ';$LastExitCode'
-    $exitCode = Invoke-Expression $expr
-    if($exitCode -ne 0){
-      Write-Host -foregroundColor red "Unable to Format - '$($file)'"
-    } else {
-      Write-Host -foregroundColor cyan "Formatted - $($file)"
-    }
-  }
-}).TotalMilliseconds
+0..9 | ForEach-Object -Parallel {
+  Write-Host -foregroundColor yellow "Kicking off emacs Process [$($_+1)/10] this will take a while!"
+  $expr = "emacs -batch -l ./scripts/emacs/load-srefactor.el 10 $($_)"
+  $exitCode = Invoke-Expression $expr
+  Write-Host -foregroundColor green "Finished Process [$($_+1)/10]"
+} -ThrottleLimit 10
