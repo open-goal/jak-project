6b6eeeb1d0eacf5019cec95215505e2d85fb6dc1
water <awaterford111445@gmail.com>
2020-11-06 21:09:56 -0500

    move GOAL code stack to GOAL memory

--- game/kernel/asm_funcs.asm
+++ game/kernel/asm_funcs.asm
@@ -133,6 +133,47 @@ _call_goal_asm_linux:
   pop r13
   ret
 
+global _call_goal_on_stack_asm_linux
+
+_call_goal_on_stack_asm_linux:
+  ;; RDI - stack pointer
+  ;; RSI - unused
+  ;; RDX - unused
+  ;; RCX - function pointer (goes in r13)
+  ;; R8  - st (goes in r14)
+  ;; R9  - off (goes in r15)
+
+  ;; x86 saved registers we need to modify for GOAL should be saved
+  push r13
+  push r14
+  push r15
+
+  ;; stash current stack pointer in rsi
+  mov rsi, rsp
+  ;; switch to new stack
+  mov rsp, rdi
+  ;; back up old stack pointer
+  push rsi
+
+  ;; set GOAL function pointer
+  mov r13, rcx
+  ;; offset
+  mov r14, r8
+  ;; symbol table
+  mov r15, r9
+  ;; call GOAL by function pointer
+  call r13
+
+  ;; get old stack pointer
+  pop rsi
+  mov rsp, rsi
+
+  ;; retore x86 registers.
+  pop r15
+  pop r14
+  pop r13
+  ret
+
 
 ;; The _call_goal_asm function is used to call a GOAL function from C.
 ;; It supports up to 3 arguments and a return value.
@@ -184,4 +225,55 @@ _call_goal_asm_win32:
   pop rbx
   pop rdx
   
+  ret
+
+global _call_goal_on_stack_asm_win32
+
+_call_goal_on_stack_asm_win32:
+  push rdx    ; 8
+  push rbx    ; 16
+  push rbp    ; 24
+  push rsi    ; 32
+  push rdi    ; 40
+  push r8     ; 48
+  push r9     ; 56
+  push r10    ; 64
+  push r11    ; 72
+  push r12    ; 80
+  push r13    ; 88
+  push r14    ; 96
+  push r15    ; 104
+
+  ;; stack swap
+  mov rsi, rsp
+  mov rsp, rdi
+  push rsi
+
+  ;mov rdi, rcx ;; rdi is GOAL first argument, rcx is windows first argument
+  ;mov rsi, rdx ;; rsi is GOAL second argument, rdx is windows second argument
+  ;mov rdx, r8  ;; rdx is GOAL third argument, r8 is windows third argument
+  mov r13, r9  ;; r13 is GOAL fp, r9 is windows fourth argument
+  mov r15, [rsp + 152] ;; symbol table
+  mov r14, [rsp + 144] ;; offset
+
+  call r13
+
+  ;; restore stack
+  pop rsi
+  mov rsp, rsi
+
+  pop r15
+  pop r14
+  pop r13
+  pop r12
+  pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdi
+  pop rsi
+  pop rbp
+  pop rbx
+  pop rdx
+
   ret
