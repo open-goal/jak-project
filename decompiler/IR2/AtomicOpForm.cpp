cc3940be6acbf66ab2cd1926e7e58ffe636f9662
water <awaterford111445@gmail.com>
2021-01-24 16:29:28 -0500

    clang format

--- decompiler/IR2/AtomicOpForm.cpp
+++ decompiler/IR2/AtomicOpForm.cpp
@@ -101,7 +101,8 @@ FormElement* LoadVarOp::get_as_form(FormPool& pool, const Env& env) const {
         for (auto& x : rd.tokens) {
           tokens.push_back(to_token(x));
         }
-        auto load = pool.alloc_single_element_form<DerefElement>(nullptr, source, rd.addr_of, tokens);
+        auto load =
+            pool.alloc_single_element_form<DerefElement>(nullptr, source, rd.addr_of, tokens);
         return pool.alloc_element<SetVarElement>(m_dst, load, true);
       }
 
