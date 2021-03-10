9430b4772a589b2cbc78b11870796d408fc9e996
ManDude <7569514+ManDude@users.noreply.github.com>
2021-03-10 04:51:28 +0000

    Implement runtime display (test) (#318)
    * Implement runtime display (test)
    
    * Update runtime.cpp
    
    * [game display] add "-nodisplay" argument
    
    * style fixes
    
    * Update gfx.cpp
    
    * [deci2server] fix deadlock when killing a Deci2Server
    
    * add libxrandr to linux github test
    
    * correct package name to libxrandr-dev
    
    * set g_main_thread_id in exec_runtime
    
    * add libxinerama to linux test packages
    
    * correct the name
    
    * add libxcursor1 package
    
    * Update linux-workflow.yaml
    
    * add libxi-dev
    
    * fix constructor for g_main_thread_id
    
    * fix submodules + use -nodisplay during tests
    
    * move the gfx loop to its own function and use a lambda for exit conditions
    
    * fix include
    
    * fix include
    
    * fix includes (for real this time)

---
+++ game/graphics/display.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/*!
+ * @file display.h
+ * Display for graphics. This is the game window, distinct from the runtime console.
+ */
+
+#ifndef RUNTIME_DISPLAY_H
+#define RUNTIME_DISPLAY_H
+
+#include "opengl.h"
+
+namespace Display {
+
+// TODO - eventually we might actually want to support having multiple windows and viewpoints
+// so it would be nice if we didn't end up designing this system such that this MUST be
+// a single window.
+extern GLFWwindow* display;
+
+void InitDisplay(int width, int height, char* title, GLFWwindow*& d);
+void KillDisplay(GLFWwindow*& d);
+
+}  // namespace Display
+
+#endif  // RUNTIME_DISPLAY_H
