8d99bee88c52b45d46419b46da52cd10be1a85e8
ManDude <7569514+ManDude@users.noreply.github.com>
2021-05-05 02:36:22 +0100

    [decompile] decomp `time-of-day-h`, fix `mood-h`, update `sky-h`, make `art-h` almost work (#415)
    * [decompiler] make certain floats decompile to constants
    
    * update various types in `mood-h`, `time-of-day-h` and `art-h`
    
    * fix `time-of-day-context`
    
    * add some offline tests
    
    * make `new-dynamic-structure` decompile at least
    
    * formatting
    
    * Update all-types.gc
    
    * Update all-types.gc

--- common/type_system/TypeSystem.cpp
+++ common/type_system/TypeSystem.cpp
@@ -962,7 +962,7 @@ int TypeSystem::get_size_in_type(const Field& field) const {
     if (field.is_inline()) {
       if (!allow_inline(field_type)) {
         fmt::print(
-            "[Type System] Attempted to use {} inline, this probably isn't what you wanted.\n",
+            "[Type System] Attempted to use `{}` inline, this probably isn't what you wanted.\n",
             field_type->get_name());
         throw std::runtime_error("bad get size in type");
       }
@@ -982,7 +982,8 @@ int TypeSystem::get_size_in_type(const Field& field) const {
     if (field.is_inline()) {
       if (!allow_inline(field_type)) {
         fmt::print(
-            "[Type System] Attempted to use {} inline, this probably isn't what you wanted.\n",
+            "[Type System] Attempted to use `{}` inline, this probably isn't what you wanted. Type "
+            "may not be defined fully.\n",
             field_type->get_name());
         throw std::runtime_error("bad get size in type");
       }
