7d6931155271bee7ab23dee644c7190d2a0cbd72
ManDude <7569514+ManDude@users.noreply.github.com>
2021-05-13 02:45:36 +0100

    [decomp] some of `fact-h` (#473)
    * update scripts
    
    * decomp some of `fact-h`

--- scripts/batch/repo-settings-mark.bat
+++ scripts/batch/repo-settings-mark.bat
@@ -1,2 +1,2 @@
 cd ..\..
-git update-index --assume-unchanged decompiler\config\jak1_ntsc_black_label.jsonc
+git update-index --assume-unchanged decompiler\config\jak1_ntsc_black_label.jsonc decompiler\config\jak1_ntsc_black_label\inputs.jsonc
