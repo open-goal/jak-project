790e65a78cc16fb4f709fd10f1b375b8cbdcc6b7
Tyler Wilding <xTVaser@users.noreply.github.com>
2021-05-13 20:46:37 -0400

    decomp: Decompile `wind` (#480)
    * decomp: Decompile `wind`
    
    * include `<limits>`

--- scripts/next-decomp-file.py
+++ scripts/next-decomp-file.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--skip", type=int)
 parser.add_argument("--file")
-parser.add_argument("--list", action="store_true")
+parser.add_argument("--list", type=int)
 args = parser.parse_args()
 
 skip_count = 0
@@ -41,7 +41,7 @@ else:
       if skip_count <= 0 and len(lines) <= 7:
         if args.list:
           list_of_eligible.append(file[0])
-          if len(list_of_eligible) >= 10:
+          if len(list_of_eligible) >= args.list:
             break
         else:
           update_file(file)
