afd63e2ed9c0cebbe6d6ef9fc3dfe4f6257f9cd0
ManDude <7569514+ManDude@users.noreply.github.com>
2021-06-23 03:34:40 +0100

    use `std::function` for this because that's what you're meant to do

--- game/graphics/gfx.cpp
+++ game/graphics/gfx.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "gfx.h"
+#include <functional>
 #include "common/log/log.h"
 #include "game/runtime.h"
 #include "display.h"
@@ -33,6 +34,30 @@ u32 Init() {
   }
 
   return 0;
+}
+
+void Loop(std::function<bool()> f) {
+  while (f()) {
+    // run display-specific things
+    if (Display::display) {
+      // lg::debug("run display");
+      glfwMakeContextCurrent(Display::display);
+
+      // render graphics
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      glfwSwapBuffers(Display::display);
+
+      // poll events TODO integrate input with cpad
+      glfwPollEvents();
+
+      // exit if display window was closed
+      if (glfwWindowShouldClose(Display::display)) {
+        // Display::KillDisplay(Display::display);
+        MasterExit = 1;
+      }
+    }
+  }
 }
 
 u32 Exit() {
