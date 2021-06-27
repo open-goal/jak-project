9f61043bf49c78541a2a54a27ea658f1ff1bf891
Tyler Wilding <xtvaser@gmail.com>
2021-06-27 17:07:07 -0400

    decomp: fix reference tests

---
+++ scripts/default-file-or-folder.py
@@ -0,0 +1,16 @@
+# Windows doesn't have the GNU toolchain by default, making it hard to write Taskfiles that are cross-platform
+# This "solves" that
+
+# Ensures it only will delete from the current directory
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--path")
+args = parser.parse_args()
+
+import os
+from os import path
+import shutil
+
+if path.exists("./{}".format(args.path)):
+  shutil.rmtree("./{}".format(args.path))
