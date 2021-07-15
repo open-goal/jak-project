d5fc05200571391b467e5ac4fd3026f64646eb87
water <awaterford111445@gmail.com>
2021-07-14 21:44:40 -0400

    fix stupid bug

--- goalc/make/Tool.cpp
+++ goalc/make/Tool.cpp
@@ -9,30 +9,38 @@
 
 Tool::Tool(const std::string& name) : m_name(name) {}
 
+namespace {
+bool invalid(const std::filesystem::path& path, const std::filesystem::file_time_type& in_time) {
+  if (std::filesystem::exists(path)) {
+    auto dep_time = std::filesystem::last_write_time(path);
+    if (dep_time < in_time) {
+      return true;
+    }
+  } else {
+    return true;
+  }
+  return false;
+}
+}  // namespace
+
 bool Tool::needs_run(const ToolInput& task) {
   auto in_time = std::filesystem::last_write_time(
       std::filesystem::path(file_util::get_file_path({task.input})));
 
   for (auto& dep : task.deps) {
-    auto dep_time =
-        std::filesystem::last_write_time(std::filesystem::path(file_util::get_file_path({dep})));
-    if (dep_time < in_time) {
+    if (invalid(file_util::get_file_path({dep}), in_time)) {
       return true;
     }
   }
 
   for (auto& out : task.output) {
-    auto dep_time =
-        std::filesystem::last_write_time(std::filesystem::path(file_util::get_file_path({out})));
-    if (dep_time < in_time) {
+    if (invalid(file_util::get_file_path({out}), in_time)) {
       return true;
     }
   }
 
   for (auto& dep : get_additional_dependencies(task)) {
-    auto dep_time =
-        std::filesystem::last_write_time(std::filesystem::path(file_util::get_file_path({dep})));
-    if (dep_time < in_time) {
+    if (invalid(file_util::get_file_path({dep}), in_time)) {
       return true;
     }
   }
