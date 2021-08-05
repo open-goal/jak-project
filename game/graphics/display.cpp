eff57e3ca35ec813ea8e49f24a3b06a10cc1622d
ManDude <7569514+ManDude@users.noreply.github.com>
2021-08-05 03:18:27 +0100

    revamp gfx and display systems a bit

--- game/graphics/display.cpp
+++ game/graphics/display.cpp
@@ -4,51 +4,120 @@
  */
 
 #include "display.h"
+#include "gfx.h"
 
 #include "common/log/log.h"
 
-namespace Display {
 
-GLFWwindow* display = NULL;
+namespace {
 
-void InitDisplay(int width, int height, const char* title, GLFWwindow*& d) {
-  if (d) {
-    lg::warn("InitDisplay has already created a display window");
-    return;
-  }
-
-  // request OpenGL 3.0 (placeholder)
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  d = glfwCreateWindow(width, height, title, NULL, NULL);
-
-  if (!d) {
-    lg::error("InitDisplay failed - Could not create display window");
-    return;
-  }
-
-  glfwMakeContextCurrent(d);
-  if (!gladLoadGL()) {
-    lg::error("GL init fail");
-    KillDisplay(d);
-    return;
-  }
-
-  // enable vsync by default
-  glfwSwapInterval(1);
-
-  lg::debug("init display #x{}", (uintptr_t)d);
+bool renderer_is_correct(const GfxRendererModule* renderer, GfxPipeline pipeline) {
+  return renderer->pipeline == pipeline;
 }
 
-void KillDisplay(GLFWwindow*& d) {
-  lg::debug("kill display #x{}", (uintptr_t)d);
-  if (!d) {
-    lg::warn("KillDisplay called when no display was available");
+void set_main_display(GfxDisplay* display) {
+  if (Display::displays.size() > 0) {
+    Display::displays[0] = display;
+  } else {
+    Display::displays.push_back(display);
+  }
+}
+
+}
+
+
+GfxDisplay::GfxDisplay(GLFWwindow* a_window) {
+  set_renderer(GfxPipeline::OpenGL);
+  set_window(a_window);
+}
+
+GfxDisplay::~GfxDisplay() {
+  renderer->kill_display(this);
+  // window_generic_ptr = nullptr;
+}
+
+void GfxDisplay::set_renderer(GfxPipeline pipeline) {
+  if (is_active()) {
+    lg::error("Can't change display's renderer while window exists.");
+    return;
+  }
+  if (renderer != nullptr) {
+    lg::error("A display changed renderer unexpectedly.");
     return;
   }
 
-  glfwDestroyWindow(d);
-  d = NULL;
+  renderer = Gfx::GetRenderer(pipeline);
+}
+
+void GfxDisplay::set_window(GLFWwindow* window) {
+  if (!renderer_is_correct(renderer, GfxPipeline::OpenGL)) {
+    lg::error("Can't set OpenGL window when using {}", renderer->name);
+    return;
+  }
+  if (is_active()) {
+    lg::error("Already have a window. Close window first.");
+    return;
+  }
+
+  this->window_glfw = window;
+}
+
+void GfxDisplay::set_title(const char* title)
+{
+  if (!is_active()) {
+    lg::error("No window to set title `{}`.", title);
+    return;
+  }
+
+  this->title = title;
+}
+
+void GfxDisplay::render_graphics() {
+  renderer->render_display(this);
+}
+
+
+namespace Display {
+
+std::vector<GfxDisplay*> displays;
+GfxDisplay* GetMainDisplay() {
+  if (displays.size() == 0)
+    return NULL;
+  return displays.front()->is_active() ? displays.front() : NULL;
+}
+
+
+int InitMainDisplay(int width, int height, const char* title, GfxSettings& settings) {
+  if (GetMainDisplay() != NULL) {
+    lg::warn("InitMainDisplay called when main display already exists.");
+    return 1;
+  }
+
+  set_main_display(settings.renderer->make_main_display(width, height, title, settings));
+}
+
+void KillDisplay(GfxDisplay* display) {
+  lg::debug("kill display #x{:x}", (uintptr_t)display);
+  if (!display->is_active()) {
+    lg::warn("display #x{:x} cant be killed because it is not active");
+    return;
+  }
+
+  if (GetMainDisplay() == display) {
+    // killing the main display, kill all children displays too!
+    for (auto child_disp : displays) {
+      if (child_disp == display)
+        continue;
+      KillDisplay(child_disp);
+    }
+  }
+
+  // find this display in the list and remove it from there
+  auto this_disp = std::find(displays.begin(), displays.end(), display);
+  displays.erase(this_disp);
+
+  // delete the display. the destructor does the rest.
+  delete display;
 }
 
 }  // namespace Display
