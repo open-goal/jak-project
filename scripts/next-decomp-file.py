df92a55749976531b98a6804b08d042cca2bdd8e
Tyler Wilding <xTVaser@users.noreply.github.com>
2021-09-19 21:45:14 -0400

    tooling: breaking off my tooling changes in the big batch PR so they can be used (#850)

--- scripts/next-decomp-file.py
+++ scripts/next-decomp-file.py
@@ -3,38 +3,39 @@ from jak1_file_list import file_list
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--file")
+parser.add_argument("--files")
 parser.add_argument("--list", type=int)
 args = parser.parse_args()
 
-def update_file(file, hack):
+def update_json_file(decomp_list):
   new_file_contents = []
-  print("Next file to decompile is - " + file[0])
-  print("Target is - " + "goal_src/" + file[4] + "/" + file[0] + ".gc")
-  print("Uses the following CGO / DGO - " + str(file[3]))
+  print("Decompiling - " + ','.join(decomp_list))
   # TODO, update the CGO/DGO
   with open("decompiler\config\jak1_ntsc_black_label.jsonc", "r") as config_file:
     cfg_lines = config_file.readlines()
     for line in cfg_lines:
-      if "allowed_objects" in line and hack == False:
-        line = "  \"allowed_objects\": [\"" + file[0] + "\"],\n"
-      elif "allowed_objects" in line and hack == True:
-        line = "  \"allowed_objects\": [\"" + file[1] + "\"],\n"
+      if "allowed_objects" in line:
+        line = "  \"allowed_objects\": [\"" + ','.join(decomp_list) + "\"],\n"
       new_file_contents.append(line)
   if len(new_file_contents) > 0:
     with open("decompiler\config\jak1_ntsc_black_label.jsonc", "w") as f:
       f.writelines(new_file_contents)
       print("Allowed objects list updated")
 
-if args.file:
-  for file in file_list:
-    if file[0] == args.file:
-      update_file(file, False)
-      break
-    elif file[1] == args.file:
-      # NOTE - kinda a hack, assumes -ag files always come after
-      update_file(file, True)
-      break
+if args.files:
+  input_list = args.files.split(",")
+  decomp_list = []
+  for inFile in input_list:
+    for file in file_list:
+      if file[0] == inFile:
+        decomp_list.append(file[0])
+        break
+      elif file[1] == inFile:
+        # NOTE - kinda a hack, assumes -ag files always come after
+        decomp_list.append(file[1])
+        break
+  if len(decomp_list) > 0:
+    update_json_file(decomp_list)
 else:
   list_eligible = []
   for file in file_list:
