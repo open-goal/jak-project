7a05cf12edfd00c19ed14d15dc605a4b74ee48f8
water <awaterford111445@gmail.com>
2021-10-01 22:26:43 -0400

    more progress

--- common/goal_constants.h
+++ common/goal_constants.h
@@ -24,7 +24,7 @@ constexpr u32 GOAL_COPY_METHOD = 6;      // method ID of GOAL copy
 constexpr u32 GOAL_RELOC_METHOD = 7;     // method ID of GOAL relocate
 constexpr u32 GOAL_MEMUSAGE_METHOD = 8;  // method ID of GOAL mem-usage
 
-constexpr int EE_MAIN_MEM_LOW_PROTECT = 1024 * 1024;
+constexpr int EE_MAIN_MEM_LOW_PROTECT = 512 * 1024;
 constexpr int EE_MAIN_MEM_SIZE = 128 * (1 << 20);  // 128 MB, same as PS2 TOOL
 constexpr u64 EE_MAIN_MEM_MAP = 0x2123000000;      // intentionally > 32-bit to catch pointer bugs
 
