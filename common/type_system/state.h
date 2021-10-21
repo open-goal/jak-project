52e07ec176cd35ccda677e1b66ac386a7dc69d47
water <awaterford111445@gmail.com>
2021-10-20 20:33:13 -0400

    clean up

--- common/type_system/state.h
+++ common/type_system/state.h
@@ -11,7 +11,7 @@ enum class StateHandler { ENTER, EXIT, CODE, TRANS, POST, EVENT };
 
 class TypeSystem;
 
-TypeSpec state_to_go_function(const TypeSpec& state_type);
+TypeSpec state_to_go_function(const TypeSpec& state_type, const TypeSpec& return_type);
 StateHandler handler_name_to_kind(const std::string& name);
 std::string handler_kind_to_name(StateHandler kind);
 TypeSpec get_state_handler_type(const std::string& handler_name, const TypeSpec& state_type);
