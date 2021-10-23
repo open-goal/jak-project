d6f631383e25a1f63364ba1a2125ad781ef197b8
water111 <48171810+water111@users.noreply.github.com>
2021-10-23 16:00:49 -0400

    decomp more level stuff (#932)
    * more level stuff
    
    * vi1 to jun load working, with multiple skies
    
    * small fixes
    
    * add level ref test
    
    * clang

--- game/kernel/klink.cpp
+++ game/kernel/klink.cpp
@@ -64,7 +64,7 @@ void link_control::begin(Ptr<uint8_t> object_file,
 
     if (link_debug_printfs) {
       char* goal_name = object_file.cast<char>().c();
-      printf("link %s\n", goal_name);
+      printf("link %s\n", m_object_name);
       printf("link_control::begin %c%c%c%c\n", goal_name[0], goal_name[1], goal_name[2],
              goal_name[3]);
     }
