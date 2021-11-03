40655f18c06ce0ca337f2c0c7337798628d41c98
ManDude <7569514+ManDude@users.noreply.github.com>
2021-11-03 16:50:51 +0000

    implement `defskelgroup` in decompiler
    0 lines removed!

---
+++ decompiler/analysis/find_skelgroups.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "decompiler/Function/Function.h"
+#include "decompiler/util/DecompilerTypeSystem.h"
+
+namespace decompiler {
+void run_defskelgroups(Function& top_level_func);
+}
