af161909e89fa4ed8c9165734bccb97e6c360481
Tyler Wilding <xtvaser@gmail.com>
2021-11-13 22:32:26 -0500

    formatting

--- common/goos/Interpreter.h
+++ common/goos/Interpreter.h
@@ -213,8 +213,8 @@ class Interpreter {
                                Arguments& args,
                                const std::shared_ptr<EnvironmentObject>& env);
   Object eval_get_env(const Object& form,
-                               Arguments& args,
-                               const std::shared_ptr<EnvironmentObject>& env);
+                      Arguments& args,
+                      const std::shared_ptr<EnvironmentObject>& env);
 
   // specials
   Object eval_define(const Object& form,
