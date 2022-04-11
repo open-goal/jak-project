4fd1db20b169ec17019197e39dabe70d5a33cb66
Tyler Wilding <xtvaser@gmail.com>
2022-04-10 21:14:02 -0400

    asserts: add `ASSERT_MSG` macro and ensure `stdout` is flushed before `abort`ing

--- common/util/Assert.cpp
+++ common/util/Assert.cpp
@@ -3,7 +3,18 @@
 
 #include "Assert.h"
 
-void private_assert_failed(const char* expr, const char* file, int line, const char* function) {
-  fprintf(stderr, "%s:%d: Assertion failed: %s\nFunction: %s\n", file, line, expr, function);
+void private_assert_failed(const char* expr,
+                           const char* file,
+                           int line,
+                           const char* function,
+                           const char* msg) {
+  if ((msg != NULL) && (msg[0] == '\0')) {
+    fprintf(stderr, "Assertion failed: '%s'\n\tSource: %s:%d\n\tFunction: %s\n", expr, file, line,
+            function);
+  } else {
+    fprintf(stderr, "Assertion failed: '%s'\n\tMessage: %s\n\tSource: %s:%d\n\tFunction: %s\n",
+            expr, msg, file, line, function);
+  }
+  fflush(stdout);  // ensure any stdout logs are flushed before we terminate
   abort();
 }
--- common/util/Assert.h
+++ common/util/Assert.h
@@ -3,12 +3,15 @@
  * Custom ASSERT macro
  */
 
+#include "third-party/fmt/core.h"
+
 #pragma once
 
 [[noreturn]] void private_assert_failed(const char* expr,
                                         const char* file,
                                         int line,
-                                        const char* function);
+                                        const char* function,
+                                        const char* msg = "");
 
 #ifdef _WIN32
 #define __PRETTY_FUNCTION__ __FUNCSIG__
@@ -16,3 +19,7 @@
 
 #define ASSERT(EX) \
   (void)((EX) || (private_assert_failed(#EX, __FILE__, __LINE__, __PRETTY_FUNCTION__), 0))
+
+#define ASSERT_MSG(EXPR, STR) \
+  (void)((EXPR) ||            \
+         (private_assert_failed(#EXPR, __FILE__, __LINE__, __PRETTY_FUNCTION__, STR.c_str()), 0))
