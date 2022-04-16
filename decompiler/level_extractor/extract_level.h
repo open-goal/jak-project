0025b5756044d7b8b4cdf1f5c25eff2a3620b061
water <awaterford111445@gmail.com>
2022-04-15 20:21:10 -0400

    faster extraction

--- decompiler/level_extractor/extract_level.h
+++ decompiler/level_extractor/extract_level.h
@@ -6,10 +6,18 @@
 #include "decompiler/ObjectFile/ObjectFileDB.h"
 
 namespace decompiler {
-void extract_from_level(ObjectFileDB& db,
-                        TextureDB& tex_db,
+void extract_from_level(const ObjectFileDB& db,
+                        const TextureDB& tex_db,
                         const std::string& dgo_name,
                         const DecompileHacks& hacks,
                         bool dump_level);
-void extract_common(ObjectFileDB& db, TextureDB& tex_db, const std::string& dgo_name);
+void extract_common(const ObjectFileDB& db, const TextureDB& tex_db, const std::string& dgo_name);
+
+// extract everything
+void extract_all_levels(const ObjectFileDB& db,
+                        const TextureDB& tex_db,
+                        const std::vector<std::string>& dgo_names,
+                        const std::string& common_name,
+                        const DecompileHacks& hacks,
+                        bool debug_dump_level);
 }  // namespace decompiler
