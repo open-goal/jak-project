6774ec2f82fa4e73a16e62241338eba68a394db9
ManDude <7569514+ManDude@users.noreply.github.com>
2022-05-28 23:09:25 +0100

    assert toggle

--- common/util/Assert.h
+++ common/util/Assert.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#ifndef NO_ASSERT
+
 #include <string_view>
 
 [[noreturn]] void private_assert_failed(const char* expr,
@@ -23,8 +25,16 @@
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
 
+
 #define ASSERT(EX) \
   (void)((EX) || (private_assert_failed(#EX, __FILE__, __LINE__, __PRETTY_FUNCTION__), 0))
 
 #define ASSERT_MSG(EXPR, STR) \
   (void)((EXPR) || (private_assert_failed(#EXPR, __FILE__, __LINE__, __PRETTY_FUNCTION__, STR), 0))
+
+#else
+
+#define ASSERT(EX) ((void)0)
+#define ASSERT_MSG(EXPR, STR) ((void)0)
+
+#endif
