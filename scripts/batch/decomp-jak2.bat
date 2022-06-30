c87e5df3de4aa7310f58e4e183a9e1e2e20a6548
ManDude <7569514+ManDude@users.noreply.github.com>
2022-06-30 04:31:07 +0100

    fix scripts for jak 2

--- scripts/batch/decomp-jak2.bat
+++ scripts/batch/decomp-jak2.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..\..
-out\build\Release\bin\decompiler decompiler\config\jak2_ntsc_v1.jsonc iso_data\jak2 decompiler_out\jak2
+out\build\Release\bin\decompiler decompiler\config\jak2_ntsc_v1.jsonc iso_data\ decompiler_out\
 pause
