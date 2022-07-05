dd4164044862d8d944242b51109c14d48cf666d8
water111 <48171810+water111@users.noreply.github.com>
2022-07-05 15:30:42 -0400

    [decompiler] missing features in inspect method detection (#1613)
    missing features in inspect method detection

--- decompiler/analysis/analyze_inspect_method.h
+++ decompiler/analysis/analyze_inspect_method.h
@@ -8,11 +8,37 @@
 
 namespace decompiler {
 
+struct TypeInspectorResult {
+  bool success = false;
+  int type_size = -1;
+  int type_method_count = -1;
+  int parent_method_count = 9;
+  int type_heap_base = -1;
+
+  std::string warnings;
+  std::vector<Field> fields_of_type;
+  bool is_basic = false;
+  bool found_flags = false;
+
+  std::string type_name;
+  std::string parent_type_name;
+  u64 flags = 0;
+
+  std::string print_as_deftype(
+      StructureType* old_game_type,
+      std::unordered_map<std::string, TypeInspectorResult>& previous_results);
+};
+
+struct TypeInspectorCache {
+  std::unordered_map<std::string, TypeInspectorResult> previous_results;
+};
+
 std::string inspect_inspect_method(Function& inspect_method,
                                    const std::string& type_name,
                                    DecompilerTypeSystem& dts,
                                    LinkedObjectFile& file,
-                                   TypeSystem& previous_game_ts);
+                                   TypeSystem& previous_game_ts,
+                                   TypeInspectorCache& ti_cache);
 
 std::string inspect_top_level_symbol_defines(std::unordered_set<std::string>& already_seen,
                                              Function& top_level,
