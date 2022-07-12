a30e90ae8618169cf6344a79bb0a3d9b39b4ecb1
Tyler Wilding <xtvaser@gmail.com>
2022-07-11 23:27:57 -0400

    lsp: remove unused third-party lib

--- third-party/json-rpc-cxx/CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Code style
-
-Please make sure your editor picks up the [.clang-format](.clang-format) file.
--- third-party/json-rpc-cxx/vendor/nlohmann/detail/meta/call_std/begin.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <nlohmann/detail/macro_scope.hpp>
-
-namespace nlohmann
-{
-NLOHMANN_CAN_CALL_STD_FUNC_IMPL(begin);
-} // namespace nlohmann
