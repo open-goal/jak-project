3ba51c8dbe9f22f5fd8bffce75fe086229e73c47
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2022-07-26 23:44:21 -0700

    move knuth-rand to separate file

--- goal_src/jak1/dgos/game.gd
+++ goal_src/jak1/dgos/game.gd
@@ -47,6 +47,7 @@
   ("connect.o" "connect")
   ("text-h.o" "text-h")
   ("settings-h.o" "settings-h")
+  ("knuth-rand.o" "knuth-rand")
   ("capture.o" "capture")
   ("memory-usage-h.o" "memory-usage-h")
   ("texture.o" "texture")
