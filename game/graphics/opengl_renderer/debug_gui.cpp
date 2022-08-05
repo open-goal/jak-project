dc6589cef588e16cb383eedc0dec1d7ca452800f
Tyler Wilding <xTVaser@users.noreply.github.com>
2022-08-05 12:12:55 -0400

    game/imgui: remove V-Sync checkbox as it can't be interacted with (#1726)

--- game/graphics/opengl_renderer/debug_gui.cpp
+++ game/graphics/opengl_renderer/debug_gui.cpp
@@ -117,8 +117,6 @@ void OpenGlDebugGui::draw(const DmaStats& dma_stats) {
     }
 
     if (ImGui::BeginMenu("Frame Rate")) {
-      ImGui::Checkbox("Enable V-Sync", &Gfx::g_global_settings.vsync);
-      ImGui::Separator();
       ImGui::Checkbox("Framelimiter", &Gfx::g_global_settings.framelimiter);
       ImGui::InputFloat("Target FPS", &target_fps_input);
       if (ImGui::MenuItem("Apply")) {
