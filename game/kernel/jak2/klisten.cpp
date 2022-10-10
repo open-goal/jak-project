439c8734fe7dcee47bbbbe3a87d69e90143de6d8
Tyler Wilding <xtvaser@gmail.com>
2022-10-10 16:16:15 -0400

    goalc: start stubbing out the SQL query handling logic

--- game/kernel/jak2/klisten.cpp
+++ game/kernel/jak2/klisten.cpp
@@ -136,7 +136,7 @@ int sql_query_sync(Ptr<String> string_in) {
         if (SqlResult->value() != s7.offset) {
           break;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));  // ?
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
         /*
         iVar2 = 99999;
         do {
