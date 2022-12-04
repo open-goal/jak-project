db228e982f2efee409da1db00d70b8d8e24f6250
Tyler Wilding <xtvaser@gmail.com>
2022-12-04 15:02:28 -0500

    common: string util namespace

---
+++ common/util/str_util.cpp
@@ -0,0 +1,13 @@
+#include "str_util.h"
+
+namespace str_util {
+int line_count(const std::string& str) {
+  int result = 0;
+  for (auto& c : str) {
+    if (c == '\n') {
+      result++;
+    }
+  }
+  return result;
+}
+}
---
+++ common/util/str_util.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace str_util {
+int line_count(const std::string& str);
+}  // namespace str_util
