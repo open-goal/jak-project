6c27870b22a69b13b7db417c19a3895636c128b8
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2023-01-13 16:13:34 +0100

    temp

--- game/graphics/opengl_renderer/OpenGLRenderer.cpp
+++ game/graphics/opengl_renderer/OpenGLRenderer.cpp
@@ -243,7 +243,8 @@ void OpenGLRenderer::init_bucket_renderers_jak2() {
   // 320
   init_bucket_renderer<DirectRenderer>("progress", BucketCategory::OTHER, BucketId::PROGRESS,
                                        0x8000);
-  init_bucket_renderer<SkipRenderer>("screen-filter", BucketCategory::OTHER, BucketId::SCREEN_FILTER);
+  init_bucket_renderer<SkipRenderer>("screen-filter", BucketCategory::OTHER,
+                                     BucketId::SCREEN_FILTER);
   init_bucket_renderer<DirectRenderer>("debug2", BucketCategory::OTHER, BucketId::DEBUG2, 0x8000);
   init_bucket_renderer<DirectRenderer>("debug-no-zbuf2", BucketCategory::OTHER,
                                        BucketId::DEBUG_NO_ZBUF2, 0x8000);
