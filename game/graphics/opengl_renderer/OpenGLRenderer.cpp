459d83f7555fafad7b3b10e221dd6950d0be07e5
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2023-01-14 19:53:31 +0100

    decomp: `tomb-*` files, `target-indax`, `grunt-mech`, `breakable-wall`, `pillar-collapse`, `mechtest-obs`, `ruins-obs` (#2114)
    Manual patches in:
    
    - `tomb-boulder`: a few `ppointer->handle`s (I think) were not being
    decompiled properly (all used in combination with `clone-anim-once`)
    - `ruins-obs`, `pillar-collapse`: `art-joint-anim` casts
    - `tomb-beetle`: commented out a call to `shadow-control-method-14` that
    was crashing the game when spawning the beetles
    - `grunt-mech`: commented out `(.mula.s)` instruction
    
    Notes:
    - `enemy-info`'s `idle-anim-script` is most likely a `(pointer
    idle-control-frame)`, however, some `nav-enemy-info` labels set it to
    `#f` (first encountered in `tomb-beetle`, but also present in `hal`,
    `roboguard` and `metalkor-setup`), which crashes the decompiler. This
    may become a problem in the future when we eventually get to these
    files. For this PR, I made `tomb-beetle` decompile with
    `idle-anim-script` set to `#f` and have not noticed any issues/crashes
    with that.
    - `tomb-boulder` compiles and doesn't crash, but when trying to play the
    Daxter chase sequence, the boulder sometimes either spawns at the origin
    or spawns in the correct place, but doesn't move.
    
    Co-authored-by: water <awaterford111445@gmail.com>

--- game/graphics/opengl_renderer/OpenGLRenderer.cpp
+++ game/graphics/opengl_renderer/OpenGLRenderer.cpp
@@ -93,7 +93,8 @@ void OpenGLRenderer::init_bucket_renderers_jak2() {
   init_bucket_renderer<TextureUploadHandler>("tex-lcom-sky-pre", BucketCategory::TEX,
                                              BucketId::TEX_LCOM_SKY_PRE);
   init_bucket_renderer<DirectRenderer>("sky-draw", BucketCategory::OTHER, BucketId::SKY_DRAW, 1024);
-  init_bucket_renderer<SkipRenderer>("bucket-6", BucketCategory::OCEAN, BucketId::OCEAN_MID_FAR);
+  init_bucket_renderer<SkipRenderer>("ocean-mid-far", BucketCategory::OCEAN,
+                                     BucketId::OCEAN_MID_FAR);
   init_bucket_renderer<TextureUploadHandler>("tex-l0-tfrag", BucketCategory::TEX,
                                              BucketId::TEX_L0_TFRAG);
   init_bucket_renderer<TFragment>("tfrag-l0-tfrag", BucketCategory::TFRAG, BucketId::TFRAG_L0_TFRAG,
@@ -163,9 +164,11 @@ void OpenGLRenderer::init_bucket_renderers_jak2() {
                                   BucketId::TFRAG_T_L1_ALPHA,
                                   std::vector{tfrag3::TFragmentTreeKind::TRANS}, false, 1);
   // 140
-  init_bucket_renderer<TextureUploadHandler>("merc-l1-alpha", BucketCategory::MERC,
-                                             BucketId::MERC_L1_ALPHA);
+  init_bucket_renderer<Merc2>("merc-l1-alpha", BucketCategory::MERC, BucketId::MERC_L1_ALPHA);
+  init_bucket_renderer<TextureUploadHandler>("tex-l2-alpha", BucketCategory::TEX,
+                                             BucketId::TEX_L2_ALPHA);
   // 150
+  init_bucket_renderer<Merc2>("merc-l2-alpha", BucketCategory::MERC, BucketId::MERC_L2_ALPHA);
   // 160
   // 170
   // 180
