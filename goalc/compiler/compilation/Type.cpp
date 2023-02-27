6f87ff1bfe0e5764082e0f58e2eff9145dcb3dc9
Tyler Wilding <xtvaser@gmail.com>
2023-02-27 16:53:53 -0500

    docs: grab file-level documentation comments

--- goalc/compiler/compilation/Type.cpp
+++ goalc/compiler/compilation/Type.cpp
@@ -613,8 +613,8 @@ Val* Compiler::compile_defmethod(const goos::Object& form, const goos::Object& _
   }
   place->set_type(lambda_ts);
 
-  auto info =
-      m_ts.define_method(symbol_string(type_name), symbol_string(method_name), lambda_ts, docstring);
+  auto info = m_ts.define_method(symbol_string(type_name), symbol_string(method_name), lambda_ts,
+                                 docstring);
   auto type_obj = compile_get_symbol_value(form, symbol_string(type_name), env)->to_gpr(form, env);
   auto id_val = compile_integer(info.id, env)->to_gpr(form, env);
   auto method_val = place->to_gpr(form, env);
