860135c91bff296b75ee6008ab82fb0a7d86337a
Tyler Wilding <xtvaser@gmail.com>
2023-02-28 22:08:26 -0500

    gk: add docs and temporary shim for launcher

--- game/main.cpp
+++ game/main.cpp
@@ -15,6 +15,8 @@
 #include "common/util/unicode_util.h"
 #include "common/versions.h"
 
+#include "game/common/game_common_types.h"
+
 #include "third-party/CLI11.hpp"
 
 #ifdef _WIN32
@@ -29,7 +31,7 @@ __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
  * @param verbose : should we print debug-level messages to stdout?
  */
 void setup_logging(bool verbose) {
-  lg::set_file(file_util::get_file_path({"log/game.txt"}));
+  // lg::set_file(file_util::get_file_path({"log/game.txt"}));
   if (verbose) {
     lg::set_file_level(lg::level::debug);
     lg::set_stdout_level(lg::level::debug);
@@ -42,30 +44,124 @@ void setup_logging(bool verbose) {
   lg::initialize();
 }
 
+std::string game_arg_documentation() {
+  // clang-format off
+  std::string output = fmt::format(fmt::emphasis::bold, "Game Args (passed through to the game runtime after '--')\n");
+  output += fmt::format(fmt::fg(fmt::color::gray), "Order matters, some args will negate others (see kmachine.cpp for details)\n");
+  output += fmt::format(fmt::fg(fmt::color::gray), "Args with `*` are not well supported\n\n");
+  // Common args
+  output += fmt::format(fmt::emphasis::bold, "Common:\n");
+  output += "  -cd          * Use the DVD drive for everything. This is how the game runs in retail\n";
+  output += "  -cddata      * Use the DVD drive for everything but IOP modules\n";
+  output += "  -deviso      * One of two modes for testing without the need for DVDs\n";
+  output += "  -fakeiso       The other of two modes for testing without the need for DVDs\n";
+  output += "  -boot          Used to set GOAL up for running the game in retail mode\n";
+  output += "  -debug         Used to set GOAL up for debugging/development\n";
+  output += "  -debug-mem     Used to set up GOAL in debug mode, but not to load debug-segments\n";
+  output += "  -nokernel      An added mode to allow booting without a KERNEL.CGO for testing\n";
+  output += "  -nosound       An added mode to allow booting without sound for testing\n";
+  // Jak 1 Related
+  output += fmt::format(fmt::emphasis::bold | fmt::fg(fmt::color::orange), "Jak 1:\n");
+  output += "  -demo          Used to pass the message `demo` to the gkernel in the DebugBootMessage (instead of play)\n";
+  output += "  -level [name]  Used to inform the game to boot a specific level the default level is `#f`\n";
+  // Jak 2 only
+  output += fmt::format(fmt::emphasis::bold | fmt::fg(fmt::color::purple), "Jak 2:\n");
+  output += "  -kiosk         A demo mode, TODO on specifics\n";
+  output += "  -preview       A demo mode, TODO on specifics\n";
+  output += "  -debug-boot    Used to boot the game in retail mode, but with debug segments\n";
+  output += "  -user [name]   Specify the debugging username, the default is `unknown`\n";
+  output += "  -art [name]    Specify the art-group name to set `DebugBootArtGroup`, there is no default\n";
+  // clang-format on
+  return output;
+}
+
 /*!
  * Entry point for the game.
  */
 int main(int argc, char** argv) {
   ArgumentGuard u8_guard(argc, argv);
 
+  // TODO - this is a temporary shim to convert the old arg format
+  // into the new
+  //
+  // This is needed to avoid a coupled release with the launcher and
+  // can be removed after one release cycle
+  //
+  // Normal users launch gk with _no_ args
+  //
+  // Only handling args the launcher provides, all others can be changed
+  // in this repo at the time of merge.
+  std::vector<char*> adjusted_argv_vals;
+  std::vector<char*> adjusted_argv_vals_passthru;
+  for (int i = 0; i < argc; i++) {
+    const auto& val = std::string(argv[i]);
+    // Handle all args that aren't passed through
+    if (val == "-proj-path") {
+      adjusted_argv_vals.push_back("--proj-path");
+      i++;
+      if (i > argc) {
+        return 1;
+      }
+      adjusted_argv_vals.push_back(argv[i]);
+    }  // now handle all the ones that get passed to the game
+    else if (val == "-boot") {
+      adjusted_argv_vals_passthru.push_back("-boot");
+    } else if (val == "-fakeiso") {
+      adjusted_argv_vals_passthru.push_back("-fakeiso");
+    } else if (val == "-debug") {
+      adjusted_argv_vals_passthru.push_back("-debug");
+    }
+  }
+
+  std::vector<char*> new_argv;
+  if (!adjusted_argv_vals.empty() || !adjusted_argv_vals_passthru.empty()) {
+    new_argv.push_back(argv[0]);
+    argc++;
+    argc = adjusted_argv_vals.size() + adjusted_argv_vals_passthru.size();
+    if (!adjusted_argv_vals_passthru.empty()) {
+      argc++;
+    }
+    for (const auto& arg : adjusted_argv_vals) {
+      new_argv.push_back(arg);
+    }
+    if (!adjusted_argv_vals_passthru.empty()) {
+      argc++;
+      new_argv.push_back("--");
+      for (const auto& arg : adjusted_argv_vals_passthru) {
+        new_argv.push_back(arg);
+      }
+    }
+    argv = new_argv.data();
+  }
+  // --- END temporary shim
+
   // CLI flags
+  std::string game_name = "jak1";
   bool verbose_logging = false;
   bool disable_avx2 = false;
+  bool disable_display = false;
+  bool disable_debug_vm = false;
   fs::path project_path_override;
   std::vector<std::string> game_args;
   CLI::App app{"OpenGOAL Game Runtime"};
+  app.add_option("-g,--game", game_name, "The game name: 'jak1' or 'jak2'");
   app.add_flag("-v,--verbose", verbose_logging, "Enable verbose logging on stdout");
   app.add_flag("--no-avx2", verbose_logging, "Disable AVX2 for testing");
+  app.add_flag("--no-display", disable_display, "Disable video display");
+  app.add_flag("--no-vm", disable_debug_vm, "Disable debug PS2 VM (defaulted to on)");
   app.add_option("--proj-path", project_path_override,
                  "Specify the location of the 'data/' folder");
-  // TODO - document the rest and add descriptions
-  app.footer(
-      "Game Args (passed through after '--'):\n  -boot\n  -debug\n  -nodisplay\n  -vm\n  -novm\n  "
-      "-jak2");
+  app.footer(game_arg_documentation());
   app.add_option("Game Args", game_args,
                  "Remaining arguments (after '--') that are passed-through to the game itself");
   app.allow_extras();
   CLI11_PARSE(app, argc, argv);
+
+  // Create struct with all non-kmachine handled args to pass to the runtime
+  GameLaunchOptions game_options;
+  game_options.disable_debug_vm = disable_debug_vm;
+  game_options.disable_display = disable_display;
+  game_options.game_version = game_name_to_version(game_name);
 
   // Figure out if the CPU has AVX2 to enable higher performance AVX2 versions of functions.
   setup_cpu_info();
@@ -107,13 +203,14 @@ int main(int argc, char** argv) {
   setup_logging(verbose_logging);
 
   bool force_debug_next_time = false;
-  std::vector<char*> arg_ptrs;
+  // always start with an empty arg, as internally kmachine starts at `1` not `0`
+  std::vector<char*> arg_ptrs = {""};
   for (auto& str : game_args) {
     arg_ptrs.push_back(str.data());
   }
+
   while (true) {
     if (force_debug_next_time) {
-      // NOTE - I presume the game doesn't care if there are duplicates?
       game_args.push_back("-boot");
       game_args.push_back("-debug");
       force_debug_next_time = false;
@@ -126,7 +223,7 @@ int main(int argc, char** argv) {
     // run the runtime in a loop so we can reset the game and have it restart cleanly
     lg::info("OpenGOAL Runtime {}.{}", versions::GOAL_VERSION_MAJOR, versions::GOAL_VERSION_MINOR);
     try {
-      auto exit_status = exec_runtime(arg_ptrs.size(), arg_ptrs.data());
+      auto exit_status = exec_runtime(game_options, arg_ptrs.size(), arg_ptrs.data());
       switch (exit_status) {
         case RuntimeExitStatus::EXIT:
           return 0;
--- game/runtime.cpp
+++ game/runtime.cpp
@@ -3,7 +3,6 @@
  * Setup and launcher for the runtime.
  */
 
-#include "common/common_types.h"
 #ifdef OS_POSIX
 #include <unistd.h>
 
@@ -23,6 +22,7 @@
 
 #include "runtime.h"
 
+#include "common/common_types.h"
 #include "common/cross_os_debug/xdbg.h"
 #include "common/goal_constants.h"
 #include "common/log/log.h"
@@ -310,25 +310,14 @@ void dmac_runner(SystemThreadInterface& iface) {
  * Main function to launch the runtime.
  * GOAL kernel arguments are currently ignored.
  */
-RuntimeExitStatus exec_runtime(int argc, char** argv) {
+RuntimeExitStatus exec_runtime(GameLaunchOptions game_options, int argc, char** argv) {
   g_argc = argc;
   g_argv = argv;
   g_main_thread_id = std::this_thread::get_id();
 
-  // parse opengoal arguments
-  g_game_version = GameVersion::Jak1;
-  bool enable_display = true;
-  for (int i = 1; i < argc; i++) {
-    if (std::string("-nodisplay") == argv[i]) {  // disable video display
-      enable_display = false;
-    } else if (std::string("-vm") == argv[i]) {  // enable debug ps2 VM
-      VM::use = true;
-    } else if (std::string("-novm") == argv[i]) {  // disable debug ps2 VM
-      VM::use = false;
-    } else if (std::string("-jak2") == argv[i]) {
-      g_game_version = GameVersion::Jak2;
-    }
-  }
+  bool enable_display = !game_options.disable_display;
+  VM::use = !game_options.disable_debug_vm;
+  g_game_version = game_options.game_version;
 
   // set up discord stuff
   gStartTime = time(nullptr);
