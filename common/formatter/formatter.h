70b8f862cc4af2b59597d52cfd74879df9276f0d
Tyler Wilding <xtvaser@gmail.com>
2023-04-23 16:17:26 -0400

    formatter: move the formatting code into `common`

---
+++ common/formatter/formatter.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace formatter {
+std::string format_code(const std::string& source);
+}
--- tools/formatter/main.cpp
+++ tools/formatter/main.cpp
@@ -18,61 +18,7 @@
 #include "third-party/CLI11.hpp"
 #include "third-party/fmt/core.h"
 #include "third-party/json.hpp"
-
-// Declare the `tree_sitter_opengoal` function, which is
-// implemented by the `tree-sitter-opengoal` library.
-extern "C" {
-extern const TSLanguage* tree_sitter_opengoal();
-}
-
-void walk_tree(TSTreeCursor* cursor, std::string& output, const std::string& source_code) {
-  // an imperative breadth-first-search
-  while (true) {
-    // Process the node
-    const auto curr_node = ts_tree_cursor_current_node(cursor);
-    const std::string curr_node_type = ts_node_type(curr_node);
-    std::string curr_node_field_name;
-    if (ts_tree_cursor_current_field_name(cursor)) {
-      curr_node_field_name = ts_tree_cursor_current_field_name(cursor);
-    }
-    if (curr_node_field_name == "open") {
-      output += "(";
-    } else if (curr_node_field_name == "close") {
-      output.pop_back();
-      output += ") ";
-    }
-    if (curr_node_type == "sym_name" || curr_node_type == "num_lit" ||
-        curr_node_type == "str_lit") {
-      uint32_t start = ts_node_start_byte(curr_node);
-      uint32_t end = ts_node_end_byte(curr_node);
-      const char* type = ts_node_type(curr_node);
-      // TODO - if it's a string literal, take out any newlines and reflow the string to the
-      // line-length
-      const auto contents = source_code.substr(start, end - start);
-      output += contents + " ";
-    }
-
-    if (ts_tree_cursor_goto_first_child(cursor)) {
-      continue;
-    }
-
-    if (ts_tree_cursor_goto_next_sibling(cursor)) {
-      continue;
-    }
-
-    while (true) {
-      if (!ts_tree_cursor_goto_parent(cursor)) {
-        if (output.at(output.length() - 1) == ' ') {
-          output.pop_back();
-        }
-        return;
-      }
-      if (ts_tree_cursor_goto_next_sibling(cursor)) {
-        break;
-      }
-    }
-  }
-}
+#include <common/formatter/formatter.h>
 
 int main(int argc, char** argv) {
   ArgumentGuard u8_guard(argc, argv);
@@ -100,33 +46,16 @@ int main(int argc, char** argv) {
   app.validate_positionals();
   CLI11_PARSE(app, argc, argv);
 
-  // Create a parser.
-  TSParser* parser = ts_parser_new();
-
-  // Set the parser's language (JSON in this case).
-  ts_parser_set_language(parser, tree_sitter_opengoal());
-
   // TODO - support recursing directories
   // Read in source code
   const auto source_code = file_util::read_text_file(file_path);
 
-  // Build a syntax tree based on source code stored in a string.
-  TSTree* tree = ts_parser_parse_string(parser, NULL, source_code.c_str(), source_code.length());
-
-  // Get the root node of the syntax tree.
-  TSNode root_node = ts_tree_root_node(tree);
-  TSNode source_node = ts_node_named_child(root_node, 0);
-
-  // Make a cursor, it's the fastest way to traverse the tree
-  TSTreeCursor cursor = ts_tree_cursor_new(source_node);
-  std::string output = "";
-  lg::info(ts_node_string(source_node));
-  walk_tree(&cursor, output, source_code);
+  const auto result = formatter::format_code(source_code);
 
   if (write_newfile) {
     // TODO - i don't like this implementation, return a new string instead
     if (str_util::replace(file_path, ".gc", ".new.gc")) {
-      file_util::write_text_file(file_path, output);
+      file_util::write_text_file(file_path, result);
     }
   }
 
