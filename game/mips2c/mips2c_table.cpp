57dbed0cf8ccbfc799118d81d8774f7b6e39c882
ManDude <7569514+ManDude@users.noreply.github.com>
2023-04-26 03:21:43 +0100

    remove jak 2 mips2c `sp-init-fields!`

--- game/mips2c/mips2c_table.cpp
+++ game/mips2c/mips2c_table.cpp
@@ -130,7 +130,6 @@ namespace render_boundary_quad { extern void link(); }
 namespace set_sky_vf27 { extern void link(); }
 namespace set_sky_vf23_value { extern void link(); }
 namespace draw_boundary_polygon { extern void link(); }
-namespace sp_init_fields { extern void link(); }
 namespace particle_adgif { extern void link(); }
 namespace sp_launch_particles_var { extern void link(); }
 namespace sparticle_motion_blur { extern void link(); }
@@ -325,7 +324,7 @@ PerGameVersion<std::unordered_map<std::string, std::vector<void (*)()>>> gMips2C
        jak2::render_boundary_quad::link, jak2::render_boundary_tri::link, jak2::set_sky_vf27::link,
        jak2::draw_boundary_polygon::link}},
      {"sparticle-launcher",
-      {jak2::sp_init_fields::link, jak2::particle_adgif::link, jak2::sp_launch_particles_var::link,
+      {jak2::particle_adgif::link, jak2::sp_launch_particles_var::link,
        jak2::sparticle_motion_blur::link}},
      {"sparticle", {jak2::sp_process_block_2d::link, jak2::sp_process_block_3d::link}},
      {"sky-tng",
