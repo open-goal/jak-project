a98eb42a40ac75eef53d053508b4d31e46ac3e17
Tyler Wilding <xtvaser@gmail.com>
2023-05-20 21:15:21 -0400

    lsp: no-op `textDocument/documentColor`

--- lsp/handlers/text_document/document_color.h
+++ lsp/handlers/text_document/document_color.h
@@ -33,39 +33,45 @@ std::optional<json> document_color_handler(Workspace& workspace, int id, json ra
   auto params = raw_params.get<LSPSpec::DocumentColorParams>();
   json colors = json::array();
 
-  // Iterate through document, mark text colors ourselves
-  auto file_type = workspace.determine_filetype_from_uri(params.textDocument.m_uri);
-
-  if (file_type == Workspace::FileType::OpenGOAL) {
-    auto tracked_file = workspace.get_tracked_og_file(params.textDocument.m_uri);
-    if (!tracked_file) {
-      return {};
-    }
-
-    // This is something that is ok to be a regex, because it's very niche
-    for (int i = 0; i < tracked_file->m_lines.size(); i++) {
-      const auto& line = tracked_file->m_lines.at(i);
-      std::smatch matches;
-      std::regex regex("\\(font-color ([^)]*)\\)");
-
-      std::sregex_iterator iter(line.begin(), line.end(), regex);
-      std::sregex_iterator end;
-
-      for (; iter != end; iter++) {
-        std::smatch match = *iter;
-        std::string capture_group = match.str(1);
-        LSPSpec::ColorInformation color_info;
-        color_info.range = {{i, match.position(1)}, {i, match.position(1) + match.size()}};
-        const auto color = color_hexstring_to_lsp_color(capture_group);
-        if (!color) {
-          continue;
-        }
-        color_info.color = color.value();
-        colors.push_back(color_info);
-        lg::debug("color - {}", capture_group);
-      }
-    }
-  }
-
+  // TODO - hex strings aren't desirable in the `font-color` enum
+  // this could be used for the `new 'static 'rgba` instances but that requires proper
+  // AST support as it cannot (and should not) be assumed that all 4 components will be on the same
+  // line
   return colors;
+
+  //// Iterate through document, mark text colors ourselves
+  // auto file_type = workspace.determine_filetype_from_uri(params.textDocument.m_uri);
+
+  // if (file_type == Workspace::FileType::OpenGOAL) {
+  //   auto tracked_file = workspace.get_tracked_og_file(params.textDocument.m_uri);
+  //   if (!tracked_file) {
+  //     return {};
+  //   }
+
+  //  // This is something that is ok to be a regex, because it's very niche
+  //  for (int i = 0; i < tracked_file->m_lines.size(); i++) {
+  //    const auto& line = tracked_file->m_lines.at(i);
+  //    std::smatch matches;
+  //    std::regex regex("\\(font-color ([^)]*)\\)");
+
+  //    std::sregex_iterator iter(line.begin(), line.end(), regex);
+  //    std::sregex_iterator end;
+
+  //    for (; iter != end; iter++) {
+  //      std::smatch match = *iter;
+  //      std::string capture_group = match.str(1);
+  //      LSPSpec::ColorInformation color_info;
+  //      color_info.range = {{i, match.position(1)}, {i, match.position(1) + match.size()}};
+  //      const auto color = color_hexstring_to_lsp_color(capture_group);
+  //      if (!color) {
+  //        continue;
+  //      }
+  //      color_info.color = color.value();
+  //      colors.push_back(color_info);
+  //      lg::debug("color - {}", capture_group);
+  //    }
+  //  }
+  //}
+
+  // return colors;
 }
