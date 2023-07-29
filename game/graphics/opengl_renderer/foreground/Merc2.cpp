50c15f1a47c368ce8a10b47eab99b515a32409b6
water111 <48171810+water111@users.noreply.github.com>
2023-07-29 19:01:55 -0400

    [jak2] disable envmap when it should be (#2864)
    In jak 2, there's an option to disable envmap. It's used on krew
    holograms, hiphog trophies, and baron bosses in palace/tomb.

--- game/graphics/opengl_renderer/foreground/Merc2.cpp
+++ game/graphics/opengl_renderer/foreground/Merc2.cpp
@@ -538,8 +538,9 @@ void Merc2::handle_pc_model(const DmaTransfer& setup,
   u64 current_ignore_alpha_bits = flags->ignore_alpha_mask;  // shader settings
   u64 current_effect_enable_bits = flags->enable_mask;       // mask for game to disable an effect
   bool model_uses_mod = flags->bitflags & 1;  // if we should update vertices from game.
-  bool model_disables_fog = (flags->bitflags & 2);
+  bool model_disables_fog = flags->bitflags & 2;
   bool model_uses_pc_blerc = flags->bitflags & 4;
+  bool model_disables_envmap = flags->bitflags & 8;
   input_data += 32;
 
   float blerc_weights[kMaxBlerc];
@@ -570,7 +571,7 @@ void Merc2::handle_pc_model(const DmaTransfer& setup,
   // stats
   stats->num_models++;
   for (const auto& effect : model_ref->model->effects) {
-    bool envmap = effect.has_envmap;
+    bool envmap = effect.has_envmap && !model_disables_envmap;
     stats->num_effects++;
     stats->num_predicted_draws += effect.all_draws.size();
     if (envmap) {
@@ -623,7 +624,7 @@ void Merc2::handle_pc_model(const DmaTransfer& setup,
     bool ignore_alpha = !!(current_ignore_alpha_bits & (1ull << ei));
     auto& effect = model->effects[ei];
 
-    bool should_envmap = effect.has_envmap;
+    bool should_envmap = effect.has_envmap && !model_disables_envmap;
     bool should_mod = (model_uses_pc_blerc || model_uses_mod) && effect.has_mod_draw;
 
     if (should_mod) {
