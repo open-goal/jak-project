73abc71177273f125a3b90faa5aea3b72f2acfeb
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2023-08-27 18:32:01 +0200

    add models and textures to custom level FR3

--- goalc/build_level/build_level.h
+++ goalc/build_level/build_level.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+#include "decompiler/level_extractor/extract_level.h"
+
 bool run_build_level(const std::string& input_file,
                      const std::string& bsp_output_file,
                      const std::string& output_prefix);
