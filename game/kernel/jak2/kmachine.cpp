d19025b398189cfae58a16b98dea5e8df83e454f
Tyler Wilding <xtvaser@gmail.com>
2023-09-10 15:50:19 -0400

    g/j2: support all font-colors in text encoding, add a way to encode utf8 text in game

--- game/kernel/jak2/kmachine.cpp
+++ game/kernel/jak2/kmachine.cpp
@@ -741,6 +741,12 @@ inline u64 bool_to_symbol(const bool val) {
 //  return bool_to_symbol(false);
 //}
 
+void convert_utf8_temp_string_to_game(u32 src_str_ptr) {
+  auto str = std::string(Ptr<String>(src_str_ptr).c()->data());
+  std::string converted = get_font_bank(GameTextVersion::JAK2)->convert_utf8_to_game(str);
+  intern_from_c("*pc-encoded-temp-string*")->value() = make_string_from_c(converted.c_str());
+}
+
 void InitMachine_PCPort() {
   // PC Port added functions
   init_common_pc_port_functions(
@@ -756,6 +762,8 @@ void InitMachine_PCPort() {
   make_function_symbol_from_c("__pc-set-levels", (void*)pc_set_levels);
   make_function_symbol_from_c("__pc-get-tex-remap", (void*)lookup_jak2_texture_dest_offset);
   make_function_symbol_from_c("pc-init-autosplitter-struct", (void*)init_autosplit_struct);
+  make_function_symbol_from_c("pc-convert-utf8-temp-string-to-game",
+                              (void*)convert_utf8_temp_string_to_game);
 
   // discord rich presence
   make_function_symbol_from_c("pc-discord-rpc-update", (void*)update_discord_rpc);
