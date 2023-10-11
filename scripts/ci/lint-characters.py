5fb5c702561e88f0ac8b40b867e6aae045805c13
Tyler Wilding <xtvaser@gmail.com>
2023-10-11 19:48:29 -0400

    scripts: allow for `_` in translation linter, hopefully people know what it does when they use it

--- scripts/ci/lint-characters.py
+++ scripts/ci/lint-characters.py
@@ -14,6 +14,7 @@ args = parser.parse_args()
 
 # fmt: off
 JAK1_ALLOWED_CHARACTERS = [
+    "_", # NOTE - not an actual underscore, adds a long space!
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     "'", "!", "(", ")", "+", "-", ",", ".", "/", ":", "=", "<", ">", "*", "%", "?", "\"",
