8d1fec34b43f77996b04cb7d24146a86d8209ecc
ManDude <7569514+ManDude@users.noreply.github.com>
2023-11-04 18:25:13 +0000

    allow per-file override of type-to-artgroup map +  use correct divide by zero handler in `civilian.gc` (#3148)

--- decompiler/config.h
+++ decompiler/config.h
@@ -171,6 +171,8 @@ struct Config {
   DecompileHacks hacks;
 
   std::unordered_map<std::string, std::string> art_group_type_remap;
+  std::unordered_map<std::string, std::unordered_map<std::string, std::string>>
+      art_group_file_override;
   std::unordered_map<std::string, std::unordered_map<int, std::string>> art_group_info_dump;
   std::unordered_map<std::string, std::unordered_map<int, std::string>> jg_info_dump;
   std::unordered_map<std::string, std::string> joint_node_hacks;
