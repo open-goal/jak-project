c297b10726256b80989f987ff7aabdf268ea86b4
Tyler Wilding <xtvaser@gmail.com>
2024-03-07 23:07:43 -0500

    game: dont spam resetting mouse camera movement if enabled

--- game/system/hid/devices/mouse.h
+++ game/system/hid/devices/mouse.h
@@ -59,6 +59,8 @@ class MouseDevice : public InputDevice {
   // Tracking motion
   int m_last_xcoord = 0;
   int m_last_ycoord = 0;
+  bool m_mouse_moved_x = false;
+  bool m_mouse_moved_y = false;
   int m_frame_counter = 0;
 
   bool is_action_already_active(const u32 sdl_keycode, const bool player_movement);
