541815f809ce18c8ab85b58bb26b5dd83ffab66e
Ziemas <ziemas@ziemas.se>
2024-05-01 06:48:43 +0200

    start all rpc threads

--- game/overlord/jak3/iso.cpp
+++ game/overlord/jak3/iso.cpp
@@ -7,16 +7,21 @@
 #include "game/common/dgo_rpc_types.h"
 #include "game/overlord/jak3/basefilesystem.h"
 #include "game/overlord/jak3/iso_fake.h"
+#include "game/overlord/jak3/stream.h"
 
 namespace jak3 {
 using namespace iop;
 
+CBaseFileSystem* g_pFileSystem;
+int g_nDGOThreadID;
+int g_nSTRThreadID;
+int g_nPlayThreadID;
+
 static int s_nISOInitFlag;
 static ISO_LoadDGO s_LoadDGO;
-int s_nSyncMbx;
-CBaseFileSystem* g_pFileSystem;
-MsgPacket s_MsgPacket_NotOnStackSync[2];
-RPC_Dgo_Cmd s_aISO_RPCBuf[1];
+static int s_nSyncMbx;
+static MsgPacket s_MsgPacket_NotOnStackSync[2];
+static RPC_Dgo_Cmd s_aISO_RPCBuf[1];
 
 static void* RPC_DGO(u32 fno, void* data, int size) {
   lg::error("RPC_DGO UNIMPLEMENTED");
@@ -73,6 +78,31 @@ int InitISOFS(const char* fs_mode, const char* loading_sceeen) {
   memset(&s_LoadDGO, 0, sizeof(s_LoadDGO));
   s_nISOInitFlag = 1;
   g_pFileSystem = &g_FakeISOCDFileSystem;
+
+  thp.attr = TH_C;
+  thp.entry = DGOThread;
+  thp.initPriority = 0x38;
+  thp.option = 0;
+  thp.stackSize = 0x900;
+  g_nDGOThreadID = CreateThread(&thp);
+
+  thp.attr = TH_C;
+  thp.entry = STRThread;
+  thp.initPriority = 0x39;
+  thp.option = 0;
+  thp.stackSize = 0x900;
+  g_nSTRThreadID = CreateThread(&thp);
+
+  thp.attr = TH_C;
+  thp.entry = PLAYThread;
+  thp.initPriority = 0x35;
+  thp.option = 0;
+  thp.stackSize = 0x900;
+  g_nPlayThreadID = CreateThread(&thp);
+
+  StartThread(g_nDGOThreadID, 0);
+  StartThread(g_nSTRThreadID, 0);
+  StartThread(g_nPlayThreadID, 0);
 
   return s_nISOInitFlag;
 }
