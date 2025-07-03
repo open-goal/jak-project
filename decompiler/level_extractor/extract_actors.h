7320bfc068acfa385f929b176f61caf3b7aabbbe
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2025-07-02 20:19:21 -0700

    g/jak1: Extract ambient data to json (#3945)
    I added extraction of ambients to json files when extracting the levels.
    All of the ambient json files are written to the same folder as actors
    are. Ambients aren't used in jak2 and 3 so it only is used on jak1.
    
    
    ![image](https://github.com/user-attachments/assets/26e5d655-6a31-49eb-8514-3374b41f72dd)
    
    ---------
    
    Co-authored-by: Tyler Wilding <xtvaser@gmail.com>

--- decompiler/level_extractor/extract_actors.h
+++ decompiler/level_extractor/extract_actors.h
@@ -5,5 +5,6 @@
 namespace decompiler {
 
 std::string extract_actors_to_json(const level_tools::DrawableInlineArrayActor& actors);
+std::string extract_ambients_to_json(const level_tools::DrawableInlineArrayAmbient& actors);
 
-}
+}  // namespace decompiler
