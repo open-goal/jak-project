4c6d3c626bdf623ee4b0ebff7438f01d6e0ac680
Matt Dallmeyer <mpdmeyer@vt.edu>
2025-10-14 17:59:49 -0700

    [jak3] Collision Renderer - support new pat-events and pat-materials

--- game/graphics/opengl_renderer/CollideMeshRenderer.h
+++ game/graphics/opengl_renderer/CollideMeshRenderer.h
@@ -4,9 +4,9 @@
 #include "game/graphics/opengl_renderer/BucketRenderer.h"
 
 struct PatColors {
-  math::Vector4f pat_mode_colors[4];
-  math::Vector4f pat_material_colors[32];
-  math::Vector4f pat_event_colors[32];
+  math::Vector4f pat_mode_colors[4]; // PAT_MOD_COUNT
+  math::Vector4f pat_material_colors[34]; // PAT_MAT_COUNT
+  math::Vector4f pat_event_colors[20]; // PAT_EVT_COUNT
 };
 
 class CollideMeshRenderer {
