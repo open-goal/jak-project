0385c768118e0a1d6c66b923cb76be07fe0ec550
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2025-11-20 16:22:29 +0100

    decomp: support `part-tracker-spawn` in jak2, part group constants (#4082)

--- decompiler/analysis/find_defpartgroup.h
+++ decompiler/analysis/find_defpartgroup.h
@@ -3,5 +3,6 @@
 #include "decompiler/Function/Function.h"
 
 namespace decompiler {
-void run_defpartgroup(Function& top_level_func);
+void run_defpartgroup(Function& top_level_func,
+                      std::unordered_map<u32, std::string>& part_group_table);
 }
