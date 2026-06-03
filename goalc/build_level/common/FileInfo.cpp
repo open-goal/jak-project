3453a24e139357ee9cd08e9e5b708a3d56b40a34
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2026-06-03 15:49:52 +0200

    add timestamp

--- goalc/build_level/common/FileInfo.cpp
+++ goalc/build_level/common/FileInfo.cpp
@@ -1,8 +1,16 @@
 #include "FileInfo.h"
 
+#include <chrono>
+
 #include "common/versions/versions.h"
 
 #include "goalc/data_compiler/DataObjectGenerator.h"
+
+std::string get_current_time_and_date() {
+  auto const now = std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now());
+  auto const time = std::chrono::current_zone()->to_local(now);
+  return std::format("{:%a %b %d %H:%M:%S %Y}", time);
+}
 
 size_t FileInfo::add_to_object_file(DataObjectGenerator& gen) const {
   gen.align_to_basic();
@@ -13,8 +21,8 @@ size_t FileInfo::add_to_object_file(DataObjectGenerator& gen) const {
   gen.add_word(major_version);
   gen.add_word(minor_version);
   gen.add_ref_to_string_in_pool(maya_file_name);
-  gen.add_ref_to_string_in_pool(tool_debug);
-  gen.add_ref_to_string_in_pool(tool_debug);
+  gen.add_ref_to_string_in_pool(tool_debug + " " + get_current_time_and_date() + "\n");
+  gen.add_ref_to_string_in_pool(mdb_file_name);
 
   return offset;
 }
